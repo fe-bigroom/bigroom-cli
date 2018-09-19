4520953daa410de312cdb3e45966fdb9bca9321c
lzwai <kevinlzwai@gmail.com>
2018-09-20 00:24:00 +0800

    update cnpmConfig

--- configs/index.js
+++ configs/index.js
@@ -3,5 +3,5 @@ const HOMEDIR = require('os').homedir()
 module.exports = {
   downloadTemp: `${HOMEDIR}/.bigroom-templates`,
   gitAccount: 'fe-bigroom',
-  cnpmConfig: ['--registry=https://registry.npm.taobao.org', '--cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist', '--userconfig=$HOME/.cnpmrc']
+  cnpmConfig: ['--registry=https://registry.npm.taobao.org', '--cache=$HOME/.npm/.cache/cnpm', '--disturl=https://npm.taobao.org/dist', '--userconfig=$HOME/.cnpmrc']
 }
